a1a36f6c551a1aa2d8dbb0193dc8dbc82ee32b76
chicken-nuggety <jayvan_leong@live.com.sg>
2024-04-01 16:01:55 +0800

    [Backend] chore: Removed unnecessary files.

--- backend/src/main/java/com/restaurant/picker/application/package-info.java
+++ backend/src/main/java/com/restaurant/picker/application/package-info.java
@@ -1,6 +1,5 @@
-@QueryEntities(value = {EProfile.class, ERestaurant.class})
+@QueryEntities(value = {ERestaurant.class})
 package com.restaurant.picker.application;
 
 import com.restaurant.picker.application.entity.ERestaurant;
 import com.querydsl.core.annotations.QueryEntities;
-import com.restaurant.picker.application.entity.EProfile;
